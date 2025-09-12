cef61df79e4f19de778d09a4a7c7424ae4d9b721
GreenMan1812 <101330476+GreenMan1812@users.noreply.github.com>
2025-09-12 07:52:19 +0300

    v1.0 - final

--- main.c
+++ main.c
@@ -4,33 +4,24 @@
 #include <util/delay.h>
 const uint8_t key2digit[] = {0x3F, 0x06, 0x5B, 0x4F,
                              0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0b01000000, 0};
-uint8_t angleInArr[] = {11, 11, 11, 1, 2};
-int16_t angle = 12;
-// #define SS_IND(IND_N, BITS){
-//     PORTA = (1 << IND_N);
-//     _delay_us(5);
-//     PORTD = BITS;
-//     _delay_us(5);
-//     PORTA = 0;
-//     _delay_us(5);
-// }
+uint8_t angleInArr[] = {11, 11, 11, 11, 0};
+int16_t angle = 0;
 void SS_IND_NEW(uint8_t IND_N, uint8_t DIGIT)
 {    
     PORTC = key2digit[DIGIT];
 
     PORTA = (1 << IND_N);
-    _delay_us(500);
+    _delay_us(25);
     PORTA = 0;
 }
 
-bool STOP_FLAG = true;
 void resetForNull();
 void num2digitArr();
 void showNumber();
 
 int main(void)
 {
-    // EIMSK = (1 << INT2) | (1 << INT1);
+    EIMSK = (1 << INT2) | (1 << INT1);
     EICRA = (1 << ISC11) | (1 << ISC11) | (1 << ISC21) | (1 << ISC20);
     // настройка портов выхода для семисегментника
     DDRA = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); // это про номер индикаторов
@@ -44,8 +35,7 @@ int main(void)
         // cnt+=1;
         // if(!STOP_FLAG){
         // cli();
-        showNumber(angle++);
-        _delay_ms(1000);
+        showNumber();
         // STOP_FLAG = true;
         // sei();
         // }
@@ -57,32 +47,35 @@ void showNumber()
     num2digitArr();
     bool isFirstDigit = false;
     // uint8_t ind_n = 0;
-    for (uint8_t i = 1; i <= 4; i++)
-    {
-        SS_IND_NEW(i + 1, angleInArr[i]);
-        uint8_t ind_n = i + 1;
-        ind_n = i + 1;
-        if (!isFirstDigit)
-        { // если еще не нашли первый элемент
-            if (angleInArr[i])
-            {                        // и если текущий элемент не ноль
-                isFirstDigit = true; // говорим что нашли первый элемент
-                SS_IND_NEW(ind_n, angleInArr[i]);
-                SS_IND_NEW(ind_n - 1, angleInArr[0]);
+    if(angle == 0){
+        resetForNull();
+    }
+    else{
+        for (uint8_t i = 1; i <= 4; i++)
+        {
+            SS_IND_NEW(i + 1, angleInArr[i]);
+            uint8_t ind_n = i + 1;
+            ind_n = i + 1;
+            if (!isFirstDigit)
+            { // если еще не нашли первый элемент
+                if (angleInArr[i])
+                {                        // и если текущий элемент не ноль
+                    isFirstDigit = true; // говорим что нашли первый элемент
+                    SS_IND_NEW(ind_n, angleInArr[i]);
+                    SS_IND_NEW(ind_n - 1, angleInArr[0]);
+                }
+                else
+                {                          // и если текущий элемент ноль
+                    SS_IND_NEW(ind_n, 11); // гасим все светодиоды
+                }
             }
             else
-            {                          // и если текущий элемент ноль
-                SS_IND_NEW(ind_n, 11); // гасим все светодиоды
+            {                                     // если уже нашли первый элемент
+                SS_IND_NEW(ind_n, angleInArr[i]); // просто выводим согласно hashMap
             }
         }
-        else
-        {                                     // если уже нашли первый элемент
-            SS_IND_NEW(ind_n, angleInArr[i]); // просто выводим согласно hashMap
-            // _delay_ms(10);
-        }
-        // _delay_us(10);
-        // // PORTA = 0;
     }
+    _delay_ms(25);
 }
 void num2digitArr()
 {
@@ -142,5 +135,6 @@ ISR(INT1_vect)
         else
             angle++;
     }
+    if(angle > 100 || angle < -100) angle = 100;
     // STOP_FLAG = false;
 }
